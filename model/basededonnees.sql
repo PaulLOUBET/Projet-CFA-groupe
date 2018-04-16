f6c81234a028c34f10f91f27abab0c80bb2b6a26
Jean Loubet <jeanloubet@MacBook-Air-de-Jean-2.local>
2018-04-16 14:57:02 +0200

    base

--- model/basededonnees.sql
+++ model/basededonnees.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le :  jeu. 05 avr. 2018 à 14:38
+-- Généré le :  lun. 16 avr. 2018 à 12:36
 -- Version du serveur :  5.6.38
 -- Version de PHP :  7.2.1
 
@@ -17,6 +17,16 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `Amis`
+--
+
+CREATE TABLE `Amis` (
+  `amis` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `commentaire`
 --
 
@@ -24,6 +34,43 @@ CREATE TABLE `commentaire` (
   `id` int(100) NOT NULL,
   `content` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `publication`
+--
+
+CREATE TABLE `publication` (
+  `id` int(100) NOT NULL,
+  `date` date NOT NULL,
+  `video` varchar(1000) NOT NULL,
+  `picture` varchar(1000) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(10) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(300) DEFAULT NULL,
+  `profile_pic` text,
+  `sex` varchar(10) DEFAULT NULL,
+  `description` text,
+  `birthdate` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `password`, `profile_pic`, `sex`, `description`, `birthdate`) VALUES
+(1, 'jean', NULL, NULL, 'gros', NULL, NULL);
 
 --
 -- Index pour les tables déchargées
@@ -36,6 +83,18 @@ ALTER TABLE `commentaire`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `publication`
+--
+ALTER TABLE `publication`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
@@ -43,4 +102,10 @@ ALTER TABLE `commentaire`
 -- AUTO_INCREMENT pour la table `commentaire`
 --
 ALTER TABLE `commentaire`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `publication`
+--
+ALTER TABLE `publication`
   MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
